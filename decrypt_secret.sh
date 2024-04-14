68d4aef7b3199a48b6ac4f8041931b1c331c68c1
sashafyodorov7 <sashafyodorov7@gmail.com>
2024-04-14 14:53:21 +0300

    Updated decr script

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -4,5 +4,5 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="GOOGLE_API_PW" \
+gpg --quiet --batch --yes --decrypt --passphrase="$GOOGLE_API_PW" \
 --output $HOME/secrets/credentials.json credentials.json.gpg
