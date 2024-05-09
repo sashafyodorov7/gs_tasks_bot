3fc7120def12cadd032b443b36e48ae2cbf1d86a
sashafyodorov7 <sashafyodorov7@gmail.com>
2024-05-09 14:55:57 +0300

    Updated code

--- main.py
+++ main.py
@@ -49,8 +49,9 @@ def format_message(tasks, task_type):
 today = datetime.now().strftime('%Y-%m-%d')
 
 # Получаем рабочие и личные задачи на сегодня
+# добавить если нужно отправлять задачи на сегодня if task[0]['dt'] == today
 work_tasks = [task[0] for task in get_tasks_from_sheet('WorkTasks')]
-personal_tasks = [task[0] for task in get_tasks_from_sheet('PersonalTasks')]
+personal_tasks = [task[0] for task in get_tasks_from_sheet('PersonalTasks') ]
 
 
 # Формируем сообщение
